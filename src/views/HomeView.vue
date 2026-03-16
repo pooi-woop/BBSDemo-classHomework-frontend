16f37d02f2cb3ae78f7dd404006fd9b593a39d91
woop <1960583030@qq.com>
2026-03-16 22:18:57 +0800

    修改天气功能，使用getWeatherByIp接口，从后端获取IP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,8 +17,16 @@ onMounted(() => {
 const fetchWeather = async () => {
   try {
     isLoading.value = true
-    // 直接调用天气接口，后端会自动获取客户端IP
-    const response = await weatherApi.getCurrentWeather()
+    // 从后端获取客户端IP
+    const ipResponse = await fetch('/api/ip')
+    if (!ipResponse.ok) {
+      throw new Error('获取IP地址失败')
+    }
+    const ipData = await ipResponse.json()
+    const userIp = ipData.ip
+    
+    // 使用IP查询天气
+    const response = await weatherApi.getWeatherByIp(userIp)
     weatherData.value = response
   } catch (err: any) {
     console.error('获取天气信息失败:', err)
